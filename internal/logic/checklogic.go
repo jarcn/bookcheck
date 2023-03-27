a3554e3735a3740a9f63217a07e3b302d857632f
chenjia <chenjia@qiyee-tech.com>
2023-03-27 15:09:50 +0800

    init

---
+++ internal/logic/checklogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"bookcheck/check"
+	"bookcheck/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CheckLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCheckLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CheckLogic {
+	return &CheckLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CheckLogic) Check(in *check.CheckReq) (*check.CheckResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &check.CheckResp{}, nil
+}
---
+++ internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "bookcheck/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
